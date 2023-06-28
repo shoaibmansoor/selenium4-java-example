5b5f79aaaee6c84e15f67f23366fa55861dca362
Shoaib Mansoor <shoaibmansoor@live.in>
2023-06-28 17:31:35 +0530

    update jar

--- run.bat
+++ run.bat
@@ -3,4 +3,4 @@ set APP_URL=https://authenticationtest.com
 set APP_USERNAME=user
 set APP_PASSWORD=pass
 mvn clean compile assembly:single
-java -jar target/testng-browserstack-1.0-SNAPSHOT-jar-with-dependencies.jar
+java -jar target/com-example-1.0-SNAPSHOT-jar-with-dependencies.jar
